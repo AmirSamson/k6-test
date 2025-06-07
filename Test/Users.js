0a3c07e94b37c9801fc8e99bc068e3826bace911
Amir Samson <amirhsamson93@gmail.com>
2025-06-08 01:31:05 +0330

    edited the USers test for using usernames in a JSON file

--- Test/Users.js
+++ Test/Users.js
@@ -6,29 +6,30 @@ import { check, sleep } from 'k6';
  * this test is for using a shared Array and reading and opening a JSON file of users
  * in order to use them for request on a website. 
  */
-const userData = new SharedArray('users', function(){
+const users = new SharedArray('user data', function(){
     return JSON.parse(open('./users.json'));
 });
 
 export let options = {
     vus: 10,
-    duration: '13s',
+    duration: '7s',
 }
 
-export default function() {
-    const RandomUser = data[Math.floor(Math.random() * data.length)];
-    console.log(`Testing login for: ${user.username}`);
-    
-    let res = http.post('https://reqres.in/api/login', JSON.stringify({
-        username: user.username,
-        password: user.password,
-    }), {
-        headers: { 'Content-Type': 'application/json' },
-    });
+export default function () {
+  const user = users[Math.floor(Math.random() * users.length)];
 
-    check(res, {
-        'status is 200': (r) => r.status === 200,
-    });
+  const res = http.post('https://dummyjson.com/auth/login', JSON.stringify({
+    username: user.username,
+    password: user.password,
+  }), {
+    headers: { 'Content-Type': 'application/json' },
+  });
 
-    sleep(1);
+  check(res, {
+    'status is 200': (r) => r.status === 200,
+    'received token': (r) => r.json('token') !== undefined,
+  });
+  console.log(`Token received: ${res.json('token')}`);
+
+  sleep(1);
 }
