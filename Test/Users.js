39268a8bccf1751cd6803e24b207eefb2325905d
Amir Samson <amirhsamson93@gmail.com>
2025-06-08 01:12:44 +0330

    edited the USers test for using usernames in a JSON file

--- Test/Users.js
+++ Test/Users.js
@@ -2,8 +2,12 @@ import http from 'k6/http';
 import {SharedArray}  from 'k6/data';
 import { check, sleep } from 'k6';
 
+/**
+ * this test is for using a shared Array and reading and opening a JSON file of users
+ * in order to use them for request on a website. 
+ */
 const userData = new SharedArray('users', function(){
-    return JSON.parse(open('./src/users.json'));
+    return JSON.parse(open('./users.json'));
 });
 
 export let options = {
@@ -15,7 +19,7 @@ export default function() {
     const RandomUser = data[Math.floor(Math.random() * data.length)];
     console.log(`Testing login for: ${user.username}`);
     
-    let res = http.post('https://example.com/login', JSON.stringify({
+    let res = http.post('https://reqres.in/api/login', JSON.stringify({
         username: user.username,
         password: user.password,
     }), {
