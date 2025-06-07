dbec19ded4a7102c5480f458807a289baffc6b03
Amir Samson <amirhsamson93@gmail.com>
2025-06-07 15:35:16 +0330

    added a new ramping and load test (with a scenario) test

--- Test/Ramping-and-Load-Test.js
+++ Test/Ramping-and-Load-Test.js
@@ -5,21 +5,21 @@ export let options = {
   scenarios: {
     ramping_load_test: {
       executor: 'ramping-vus',
-      startVUs: 0,
+      startVUs: 0,                       // we start with 0 VUs in the first place.
       stages: [
-        { duration: '30s', target: 20 }, // ramp-up
-        { duration: '1m', target: 20 },  // hold
-        { duration: '30s', target: 0 },  // ramp-down
+        { duration: '30s', target: 20 }, // ramp-up to 20 VUs.
+        { duration: '1m', target: 20 },  // holding the ramped up VUs requests.
+        { duration: '30s', target: 0 },  // ramping down the ramp ups to "0".
       ],
     },
   },
   thresholds: {
     http_req_duration: [
-      'p(90)<400',     // 90% of requests < 400ms
-      'p(99)<1000',    // 99% < 1s
+      'p(90)<400',                       // 90% of requests < 400ms
+      'p(99)<1000',                      // 99% < 1s
     ],
-    'http_req_failed': ['rate<0.01'], // <1% errors
-    'checks': ['rate>0.95'],          // 95% of checks must pass
+    'http_req_failed': ['rate<0.01'],    // <1% errors
+    'checks': ['rate>0.95'],             // 95% of checks must pass
   },
 };
 
