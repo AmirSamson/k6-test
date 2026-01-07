232af54a561e63df8d431079256144229001517c
Amir Samson <amirhsamson93@gmail.com>
2026-01-07 13:01:02 +0330

    Added a sleep in Basic-get.js file

--- Test/Basic-get.js
+++ Test/Basic-get.js
@@ -1,13 +1,14 @@
 import http from 'k6/http';
-import { check } from 'k6';
+import { check, sleep } from 'k6';
 
 export let options = {
   vus: 10, // virtual users
-  duration: '30s',
+  duration: '10s',
 };
 
 export default function () {
   let res = http.get('https://test-api.k6.io');
+  sleep(1) // this means that each request/iteration will wait for 1 second, after been executed. to mimic real user's behavior
   check(res, {
     'status is 200': (r) => r.status === 200,
   });
