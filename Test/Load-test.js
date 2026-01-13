fa593633a6cb15625170c3c7a03b90b5c8f470b3
Amir Samson <amirhsamson93@gmail.com>
2026-01-13 12:39:57 +0330

    added 3 new test files.

---
+++ Test/Load-test.js
@@ -0,0 +1,24 @@
+import http from 'k6/http';
+import { check, sleep } from 'k6';
+
+export let options={
+    stages: [
+        {   
+            duration: '5m',     // Ramp up stage
+            target: 100
+        },
+        {
+            duration: '30m',    // Hold stage
+            target: 100
+        },
+        {
+            duration: '5m',     // Ramp Down
+            target: 0
+        }
+    ]
+}
+
+export default function(){
+    http.get('https://test.k6.io');
+    sleep(1);
+}
