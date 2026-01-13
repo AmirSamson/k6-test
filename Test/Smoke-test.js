5c3f32e6d7849ed01cc4bf221b3694a1dec71270
Amir Samson <amirhsamson93@gmail.com>
2026-01-13 23:23:10 +0330

    added the assertions

--- Test/Smoke-test.js
+++ Test/Smoke-test.js
@@ -8,7 +8,7 @@ export let options={
 
 export default function(){
     http.get('https://test.k6.io');
-    sleep(1); // 
+    sleep(1); // this will tell K6 to wait for 1 sec after each request to that URL. to act like a real user. 
     http.get('https://test.k6.io/contacts.php')
     sleep(2)
     http.get('https://test.k6.io/news.php')
