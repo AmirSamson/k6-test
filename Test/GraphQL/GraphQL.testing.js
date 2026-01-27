58f16ecce48c2fda210ebdf758ead4d60f328703
Amir Samson <amirhsamson93@gmail.com>
2026-01-27 15:40:52 +0330

    added the GraphQL folder for tests on GraphQL APIs

---
+++ Test/GraphQL/GraphQL.testing.js
@@ -0,0 +1,34 @@
+import http from "k6/http";
+import { sleep } from "k6";
+
+const accessToken = "GITHUB_ACCESS_TOKEN";
+
+export default function(){
+    const query = `
+        query FindFirstIssue {
+            repository(owner:"grafana", name:"k6") {  
+                issues(first:1) {   
+                    edges {   
+                        node {  
+                            title 
+                        }   
+                    }
+                }
+            }
+        }`;
+
+    const headers = {
+        Authorization: `Bearer ${accessToken}`,
+            "Content-Type": "application/json",
+    };    
+
+    http.post(
+        "https://api.github.com/graphql",
+
+        JSON.stringify({ query: query }),
+
+        { headers: headers }
+    );
+
+    sleep(0.3);
+}
